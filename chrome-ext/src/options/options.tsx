52d8557e479d00b46ed0c08ad29590fa5fd2275b
mark <mark.henein9616@gmail.com>
2023-10-28 11:09:35 -0400

    Remade the starter code and reformatted the repo

---
+++ chrome-ext/src/options/options.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Options = () => {
+    return (
+        <div>
+            <h1 className="text-4xl">Aid-vertize</h1>
+        </div>
+    )
+};
+
+export default Options;
---
+++ chrome-ext/src/popup/popup.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './popup.css'
+
+const Popup = () => {
+    return (
+        <div>
+            <h1 className="text-4xl">Aid-vertize</h1>
+        </div>
+    )
+};
+
+export default Popup;
