856583db41413cf5d299c723c3a3219ef4ca011d
Jeffrey <jeffreyzhao2003gmail.com>
2023-11-24 03:34:55 -0500

    add desmos calculator

--- chrome-ext/src/newTab/components/widgetsBar/active/WidgetsBarActive.tsx
+++ chrome-ext/src/newTab/components/widgetsBar/active/WidgetsBarActive.tsx
@@ -2,7 +2,20 @@ import React, { useState } from 'react'
 import WidgetsNavActive from './WidgetsNavActive'
 import './WidgetsBarActive.css'
 import History from '../../../widgets/history/History'
+import Calculator from '../../../widgets/calculator/Calculator'
 import WidgetsHeader from './WidgetsHeader'
+
+
+const displayWidget = (display: string) => {
+  switch (display){
+    case "History":
+      return <History/>
+    case "Calculator":
+      return <Calculator/>
+    default:
+      <></>
+  }
+}
 
 export default function WidgetsBarActive(props) {
   return (
@@ -10,7 +23,7 @@ export default function WidgetsBarActive(props) {
       <WidgetsNavActive display={props.display} setDisplay={props.setDisplay} />
       <div className='widgets-bar-content'>
         <WidgetsHeader display={props.display} setDisplay={props.setDisplay} />
-        {(props.display == "History") ? <History /> : <></>}
+        {displayWidget(props.display)}
       </div>
     </div>
   )
---
+++ chrome-ext/src/newTab/widgets/calculator/Calculator.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Calculator = () => {
+    return (
+        <>
+            <iframe
+                className="w-full h-[90%]"
+                src="https://www.desmos.com/calculator/"
+            >
+            </iframe>
+        </>
+    )
+}
+
+export default Calculator;
